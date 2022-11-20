1f6648b0ab40794cd644a943e73f700de85c551f
Eran Turgeman <eranturgeman@Erans-MacBook-Pro-2.local>
2022-11-20 18:18:52 +0200

    added project 1

---
+++ EX1/Assets/CharacterAnimator.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public TextAsset BVHFile; // The BVH file that defines the animation and skeleton
+    public bool animate; // Indicates whether or not the animation should be running
+    public bool interpolate; // Indicates whether or not frames should be interpolated
+    [Range(0.01f, 2f)] public float animationSpeed = 1; // Controls the speed of the animation playback
+
+    public BVHData data; // BVH data of the BVHFile will be loaded here
+    public float t = 0; // Value used to interpolate the animation between frames
+    public float[] currFrameData; // BVH channel data corresponding to the current keyframe
+    public float[] nextFrameData; // BVH vhannel data corresponding to the next keyframe
+
+    public static float DIAMETER = 0.6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BVHParser parser = new BVHParser();
+        data = parser.Parse(BVHFile);
+        CreateJoint(data.rootJoint, Vector3.zero);
+        RotateTowardsVector(new Vector3(19, 38, 4));
+    }
+
+    // Returns a Matrix4x4 representing a rotation aligning the up direction of an object with the given v
+    public Matrix4x4 RotateTowardsVector(Vector3 v)
+    {
+        v = v.normalized;
+        double thetaX = 90 - (Mathf.Rad2Deg * Mathf.Atan2(v[1], v[2]));
+        Matrix4x4 Rx = MatrixUtils.RotateX((float)-thetaX);
+
+        double thetaZ = 90 - (Mathf.Rad2Deg * Mathf.Atan2(Mathf.Sqrt((v[1] * v[1]) + (v[2] * v[2])), v[0]));
+        Matrix4x4 Rz = MatrixUtils.RotateZ((float)thetaZ);
+
+        //print((Rx.inverse * Rz.inverse).MultiplyVector(new Vector3(0, 1, 0))); todo self check
+
+        return (Rz * Rx).inverse;
+    }
+
+    // Creates a Cylinder GameObject between two given points in 3D space
+    public GameObject CreateCylinderBetweenPoints(Vector3 p1, Vector3 p2, float diameter)
+    {
+        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        double distance = Vector3.Distance(p1, p2) / 2;
+        Matrix4x4 scaleMatrix = MatrixUtils.Scale(new Vector3(diameter, (float)distance, diameter));
+
+        Matrix4x4 rotationMatrix = RotateTowardsVector(p2 - p1);
+
+        Matrix4x4 translationMatrix = MatrixUtils.Translate(p2 + (p1 - p2)/2);
+
+        MatrixUtils.ApplyTransform(cylinder, translationMatrix * rotationMatrix * scaleMatrix);
+
+        return cylinder;
+    }
+
+    // Creates a GameObject representing a given BVHJoint and recursively creates GameObjects for it's child joints
+    public GameObject CreateJoint(BVHJoint joint, Vector3 parentPosition)
+    {
+        
+
+        joint.gameObject = new GameObject(joint.name);
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.parent = joint.gameObject.transform;
+        Vector3 scaleVec;
+        if (joint.name == "Head")
+        {
+            scaleVec = new Vector3(8, 8, 8);
+        }
+        else
+        {
+            scaleVec = new Vector3(2, 2, 2);
+        }
+
+        Matrix4x4 scaleMatrix = MatrixUtils.Scale(scaleVec);
+        MatrixUtils.ApplyTransform(sphere, scaleMatrix);
+
+        Vector3 newPosition = parentPosition + joint.offset;
+        Matrix4x4 translationMatrix = MatrixUtils.Translate(newPosition);
+        MatrixUtils.ApplyTransform(joint.gameObject, translationMatrix);
+        foreach (BVHJoint child in joint.children)
+        {
+            CreateJoint(child, newPosition);
+            GameObject cylinder = CreateCylinderBetweenPoints(newPosition, newPosition + child.offset, DIAMETER);
+            cylinder.transform.parent = joint.gameObject.transform;
+            
+        }
+
+
+        return joint.gameObject;
+
+    }
+
+    // Transforms BVHJoint according to the keyframe channel data, and recursively transforms its children
+    public void TransformJoint(BVHJoint joint, Matrix4x4 parentTransform)
+    {
+        if (joint.isEndSite)
+        {
+            MatrixUtils.ApplyTransform(joint.gameObject, parentTransform * MatrixUtils.Translate(joint.offset));
+            return;
+        }
+
+        Matrix4x4[] rotationMats = new Matrix4x4[3];
+        Matrix4x4[] orderdRotationMats = new Matrix4x4[3];
+        Matrix4x4 rotationMat = Matrix4x4.identity;
+        Matrix4x4 finalMat = parentTransform;
+
+        //todo check if needed
+        Vector3 curAngles = new Vector3();
+        Vector3 nextAngles = new Vector3();
+        //
+
+        if (interpolate)
+        {
+            //cur frame quaternions
+            curAngles[0] = currFrameData[joint.rotationChannels.x];
+            curAngles[1] = currFrameData[joint.rotationChannels.y];
+            curAngles[2] = currFrameData[joint.rotationChannels.z];
+
+            Vector4 curQ = QuaternionUtils.FromEuler(curAngles, joint.rotationOrder).normalized;
+
+
+            //next frame quaternions
+            nextAngles[0] = nextFrameData[joint.rotationChannels.x];
+            nextAngles[1] = nextFrameData[joint.rotationChannels.y];
+            nextAngles[2] = nextFrameData[joint.rotationChannels.z];
+
+            Vector4 nextQ = QuaternionUtils.FromEuler(nextAngles, joint.rotationOrder).normalized;
+
+            //slerp
+            Vector4 slerped = QuaternionUtils.Slerp(curQ, nextQ, t).normalized;
+            rotationMat = MatrixUtils.RotateFromQuaternion(slerped);
+        }
+        else
+        {
+            rotationMats[0] = MatrixUtils.RotateX(currFrameData[joint.rotationChannels.x]);
+            rotationMats[1] = MatrixUtils.RotateY(currFrameData[joint.rotationChannels.y]);
+            rotationMats[2] = MatrixUtils.RotateZ(currFrameData[joint.rotationChannels.z]);
+            for (int i = 0; i < 3; i++)
+            {
+                orderdRotationMats[joint.rotationOrder[i]] = rotationMats[i];
+            }
+            foreach (Matrix4x4 mat in orderdRotationMats)
+            {
+               rotationMat = rotationMat * mat;
+            }
+        }
+
+
+        //translation
+        if (joint == data.rootJoint)
+        {
+            Vector3 translationParms = new Vector3();
+            translationParms[0] = currFrameData[joint.positionChannels.x];
+            translationParms[1] = currFrameData[joint.positionChannels.y];
+            translationParms[2] = currFrameData[joint.positionChannels.z];
+            if (interpolate)
+            {
+                Vector3 translationParmsNext = new Vector3();
+                translationParmsNext[0] = nextFrameData[joint.positionChannels.x];
+                translationParmsNext[1] = nextFrameData[joint.positionChannels.y];
+                translationParmsNext[2] = nextFrameData[joint.positionChannels.z];
+                translationParms = Vector3.Lerp(translationParms, translationParmsNext, t);
+            }
+
+            finalMat = finalMat * MatrixUtils.Translate(translationParms);
+        }
+        else
+        {
+            finalMat = finalMat * MatrixUtils.Translate(joint.offset);
+        }
+
+
+        // rotation
+        finalMat = finalMat * rotationMat;
+
+        MatrixUtils.ApplyTransform(joint.gameObject, finalMat);
+        foreach (BVHJoint child in joint.children)
+        {
+            TransformJoint(child, finalMat);
+        }
+    }
+
+    // Returns the frame nunmber of the BVH animation at a given time
+    public int GetFrameNumber(float time)
+    {
+        return (int)(time / data.frameLength) % data.numFrames;
+
+    }
+
+    // Returns the proportion of time elapsed between the last frame and the next one, between 0 and 1
+    public float GetFrameIntervalTime(float time)
+    {
+        return (time % data.frameLength) / data.frameLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.time * animationSpeed;
+        if (animate)
+        {
+            int currFrame = GetFrameNumber(time);
+            t = GetFrameIntervalTime(time);
+            currFrameData = data.keyframes[currFrame];
+            if (currFrame + 1 != data.numFrames)
+            {
+                nextFrameData = data.keyframes[currFrame + 1];
+            }
+            TransformJoint(data.rootJoint, Matrix4x4.identity); //todo change parent matrix here
+        }
+
+
+    }
+}
---
+++ EX1/Assets/QuaternionUtils.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+
+public class QuaternionUtils
+{
+    // The default rotation order of Unity. May be used for testing
+    public static readonly Vector3Int UNITY_ROTATION_ORDER = new Vector3Int(1,2,0);
+
+    // Returns the product of 2 given quaternions
+    public static Vector4 Multiply(Vector4 q1, Vector4 q2)
+    {
+        return new Vector4(
+            q1.w*q2.x + q1.x*q2.w + q1.y*q2.z - q1.z*q2.y,
+            q1.w*q2.y + q1.y*q2.w + q1.z*q2.x - q1.x*q2.z,
+            q1.w*q2.z + q1.z*q2.w + q1.x*q2.y - q1.y*q2.x,
+            q1.w*q2.w - q1.x*q2.x - q1.y*q2.y - q1.z*q2.z
+        );
+    }
+
+    // Returns the conjugate of the given quaternion q
+    public static Vector4 Conjugate(Vector4 q)
+    {
+        return new Vector4(-q.x, -q.y, -q.z, q.w);
+    }
+
+    // Returns the Hamilton product of given quaternions q and v
+    public static Vector4 HamiltonProduct(Vector4 q, Vector4 v)
+    {
+        return Multiply(Multiply(q, v), Conjugate(q));
+    }
+
+    // Returns a quaternion representing a rotation of theta degrees around the given axis
+    public static Vector4 AxisAngle(Vector3 axis, float theta)
+    {
+        return new Vector4(
+            Mathf.Sin(Mathf.Deg2Rad * theta / 2) * axis.x,
+            Mathf.Sin(Mathf.Deg2Rad * theta / 2) * axis.y,
+            Mathf.Sin(Mathf.Deg2Rad * theta / 2) * axis.z,
+            Mathf.Cos(Mathf.Deg2Rad * theta / 2)).normalized;
+    }
+
+    // Returns a quaternion representing the given Euler angles applied in the given rotation order
+    public static Vector4 FromEuler(Vector3 euler, Vector3Int rotationOrder)
+    {
+        Vector4[] rotationVectors = new Vector4[3];
+        Vector4[] orderedVectors = new Vector4[3];
+        rotationVectors[0] = AxisAngle(Vector3.right, euler.x);
+        rotationVectors[1] = AxisAngle(Vector3.up, euler.y);
+        rotationVectors[2] = AxisAngle(Vector3.forward, euler.z);
+
+        for(int i = 0; i < 3; i++)
+        {
+            orderedVectors[rotationOrder[i]] = rotationVectors[i];
+        }
+
+        //todo check what is the correct return: first one is not as shown in class, secont one is
+
+        return Multiply(Multiply(orderedVectors[0], orderedVectors[1]), orderedVectors[2]);
+
+        //return Multiply(orderedVectors[2], Multiply(orderedVectors[1], orderedVectors[0]));
+        
+    }
+
+    //returns half the angle between q1 and q2
+    private static float AngleBetweenQuaternions(Vector4 q1, Vector4 q2)
+    {
+        Vector4 quatMult = Multiply(q1, Conjugate(q2));
+        float theta = 2 * Mathf.Acos(Mathf.Clamp(quatMult.w, -1, 1));
+        return theta > Mathf.PI ? (2 * Mathf.PI - theta) / 2 : theta / 2;
+
+    }
+
+    // Returns a spherically interpolated quaternion between q1 and q2 at time t in [0,1]
+    public static Vector4 Slerp(Vector4 q1, Vector4 q2, float t)
+    {
+        float theta = AngleBetweenQuaternions(q1, q2);
+
+        if (Mathf.Sin(theta) == 0)
+        {
+            return q1;
+        }
+
+        float firstParam = Mathf.Sin((1 - t) * theta) / Mathf.Sin(theta);
+        float secondParam = Mathf.Sin(t * theta) / Mathf.Sin(theta);
+        return firstParam * q1 + secondParam * q2;
+    }
+}
